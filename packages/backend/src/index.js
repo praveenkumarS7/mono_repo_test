119b0722efbf82355be5ea0bb7877cccd8f1b27a
praveenkumarS7 <praveenkumar.s@hubino.com>
2024-02-20 16:16:05 +0530

    master: backend code structure added

---
+++ packages/backend/src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const rescue = require('express-rescue');
+const routes = require('../routes/peopleRouter')
+const cors = require('cors')
+const middleware = require('../middlewares/index')
+const limiter = require('../utils/rateLimit')
+const helmet = require("helmet");
+const morgan = require("morgan")
+
+app.use(morgan("common"))
+app.use(helmet()); //for security Express.js purposes
+app.use(cors());
+app.use(limiter)
+app.use(bodyParser.json());
+
+const mainRoute = "/api/v1/";
+const peopleRoute = "people";
+
+app.use(mainRoute + peopleRoute, rescue(routes.peopleRouter))
+app.use(middleware.errorMiddleware)
+
+module.exports = app
---
+++ packages/backend/src/server.js
@@ -0,0 +1,5 @@
+const app = require('./index')
+
+app.listen(8080, ()=> {
+    console.info('Running on port 8080')
+})
